4c87065ff4e6eb8eb2df6e7553df2a778cad3f08
lsurma <kontakt@lsurma.pl>
2025-09-03 23:59:21 +0200

    wip

--- Lokalizer.Host.AzFuncAPI/Controllers/QueryController.cs
+++ Lokalizer.Host.AzFuncAPI/Controllers/QueryController.cs
@@ -3,23 +3,30 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Lokalizer.Application.Core.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lokalizer.Host.AzFuncAPI.Controllers;
 
 public class QueryController
 {
     private readonly ILogger<QueryController> _logger;
+    private readonly LokalizerDbContext _context;
 
-    public QueryController(ILogger<QueryController> logger)
+    public QueryController(ILogger<QueryController> logger, LokalizerDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     [Function("Query")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new JsonResult(new List<WeatherForecast>());
+        
+        var weatherForecasts = await _context.WeatherForecasts.ToListAsync();
+        
+        return new JsonResult(weatherForecasts);
     }
 
 }
